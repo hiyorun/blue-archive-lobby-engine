c9ddd6834c0f3408b65dc50f78069c36f390dd3a
hiyorun <hiyorrun@e.email>
2023-07-24 15:02:39 +0700

    fix: don't use fullpath

--- tools/json-init_nix.sh
+++ tools/json-init_nix.sh
@@ -3,7 +3,7 @@
 SOURCE=${1:-'models/'}
 DESTINATION=${2:-'src/assets/models.json'}
 
-paths=$(find $(pwd)/$SOURCE -name "*.skel")
+paths=$(find $SOURCE -name "*.skel")
 items=()
 for path in $paths; do
 	filename=$(/usr/bin/basename "${path%.skel}")
