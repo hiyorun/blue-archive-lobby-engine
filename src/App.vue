fa365bd4d11af5b5122f5cd0e0eb49d01aebc235
hiyorun <hiyorrun@e.email>
2023-08-05 15:37:21 +0700

    fix: UI minor adjustment

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import LobbyViewer from './components/LobbyViewer.vue'
 const open = ref(false)
 </script>
 <template>
-  <Cog6ToothIcon @click="open = true" class="h-6 w-6 m-3 text-arisu-600 dark:text-arisu-400 cursor-pointer">OPEN MODAL
+  <Cog6ToothIcon @click="open = true" class="rounded-full bg-arisu-200 dark:bg-arisu-900 absolute h-10 w-10 m-2 p-1 text-arisu-600 dark:text-arisu-400 cursor-pointer">OPEN MODAL
   </Cog6ToothIcon>
   <ModalOptions :open="open" @close="(v) => { open = false }"></ModalOptions>
   <LobbyViewer></LobbyViewer>
